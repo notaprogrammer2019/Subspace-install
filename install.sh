cb478b4d6fecee7995ea9b4272c6f402b78910eb
sadaman <47750617+notaprogrammer2019@users.noreply.github.com>
2022-05-31 21:47:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt install wget -y && wget -O subspace_node https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-node-ubuntu-x86_64-snapshot-2022-mar-09 && wget -O subspace_farmer https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09 && chmod +x subspace* && sudo mv subspace* /usr/local/bin/ && printf "[Unit]
+apt install wget -y && wget -O subspace_node https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-node-ubuntu-x86_64-gemini-1a-2022-may-31 && wget -O subspace_farmer https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-farmer-ubuntu-x86_64-gemini-1a-2022-may-31 && chmod +x subspace* && sudo mv subspace* /usr/local/bin/ && printf "[Unit]
 Description=Subspace Node
 After=network.target
 [Service]
 Type=simple
 User=root
-ExecStart=subspace_node --chain testnet --wasm-execution compiled --execution wasm --bootnodes \"/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr\" --rpc-cors all --rpc-methods unsafe --ws-external --validator --telemetry-url \"wss://telemetry.polkadot.io/submit/ 1\" --telemetry-url \"wss://telemetry.subspace.network/submit 1\" --name YOUR_SUBSPACE_NODE_NAME
+ExecStart=subspace_node --chain gemini-1 --wasm-execution compiled --execution wasm --port 30333 --rpc-cors all --rpc-methods safe --unsafe-ws-external --validator --name INSERT_NAME_NODE
 Restart=on-failure
 RestartSec=10
 [Install]

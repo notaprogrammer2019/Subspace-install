2a4ecea494b22996b33614633ecf1667b556c0b1
sadaman <47750617+notaprogrammer2019@users.noreply.github.com>
2022-06-05 23:27:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-apt install wget -y && wget -O subspace_node https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-node-ubuntu-x86_64-gemini-1a-2022-may-31 && wget -O subspace_farmer https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-farmer-ubuntu-x86_64-gemini-1a-2022-may-31 && chmod +x subspace* && sudo mv subspace* /usr/local/bin/ && printf "[Unit]
+ver='wget -qO- https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name"'
+
+apt install wget jq -y && wget -O subspace_node https://github.com/subspace/subspace/releases/download/${vers}/subspace-node-ubuntu-x86_64-${vers} && wget -O subspace_farmer https://github.com/subspace/subspace/releases/download/${vers}/subspace-farmer-ubuntu-x86_64-${vers} && chmod +x subspace* && sudo mv subspace* /usr/local/bin/ && printf "[Unit]
 Description=Subspace Node
 After=network.target
 [Service]
 Type=simple
 User=root
-ExecStart=subspace_node --chain gemini-1 --wasm-execution compiled --execution wasm --port 30333 --rpc-cors all --rpc-methods safe --unsafe-ws-external --validator --name INSERT_NAME_NODE
+ExecStart=subspace_node --chain gemini-1 --wasm-execution compiled --execution wasm --validator --pruning archive --name INSERT_NAME_NODE
 Restart=on-failure
 RestartSec=10
 [Install]
@@ -18,7 +20,7 @@ After=subspace-node.service
 [Service]
 Type=simple
 User=root
-ExecStart=subspace_farmer farm --reward-address YOUR_SUBSPACE_WALLET_ADDRESS --plot-size 50G
+ExecStart=subspace_farmer farm --reward-address YOUR_SUBSPACE_WALLET_ADDRESS --plot-size 100G
 Restart=on-failure
 RestartSec=10
 [Install]
